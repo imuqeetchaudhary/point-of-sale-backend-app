0525b21a621e4afae8855116c8408999bc0ab9dc
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2021-10-04 11:13:55 +0500

    added morgan & yup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 const allRoutes = require("./routes");
 
 const app = express();
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enable...");
+}
+
 app.use("/", allRoutes);
 
 module.exports = app;
