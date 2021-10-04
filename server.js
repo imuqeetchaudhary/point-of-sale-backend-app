d2d8b06922ea6ace703d4ca98954076ab1afc943
Jazim-Abbas <jazimabbas87@gmail.com>
2021-10-04 08:00:37 +0500

    bootstrap the app in server.js file with helper function

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const config = require("config");
+const app = require("./src/app");
 const db = require("./src/models");
 
-db.sequelize
-  .sync({ force: true })
-  .then(() => {
-    console.log("successfully run");
-  })
-  .catch((err) => {
-    console.log("error: ", err);
-  });
+async function bootstrap() {
+  console.log("Please wait for the server and db to run .......");
+  const PORT = process.env.PORT || 8000;
+
+  try {
+    await db.sequelize.sync({ force: config.get("db.forceSync") });
+    app.listen(PORT, () => {
+      console.clear();
+      console.log(`Db is connected & App is listening on the port ${PORT} ...`);
+    });
+  } catch (err) {
+    console.clear();
+    console.log("Some error while bootstrap the app ....", err);
+  }
+}
+
+bootstrap();
