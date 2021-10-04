002fecb6baa829c1e7de251d7c43afbbb57d86e2
Jazim-Abbas <jazimabbas87@gmail.com>
2021-10-04 16:22:52 +0500

    role-access-menus: catch error if role_id and menu_id is repeat. It should be unique

--- src/services/menu-access-roles/index.js
+++ src/services/menu-access-roles/index.js
@@ -1,15 +1,29 @@
 const db = require("../../models");
+const utils = require("../error-check.util");
+const Exceptions = require("../../utils/custom-exceptions");
 
 async function saveRoleMenus({ roleMenus }) {
   const _roleMenus = roleMenus.map((menu) => {
     return { ...menu, updatedBy: menu.createdBy };
   });
+  const transaction = await db.sequelize.transaction();
 
   try {
     const roleMenusInDb = await db.MenuAccessRoles.bulkCreate([..._roleMenus]);
+
+    await transaction.commit();
+
     return roleMenusInDb;
   } catch (err) {
-    console.log("error", err);
+    await transaction.rollback();
+
+    if (utils.isRecordDuplicate(err)) {
+      throw new Exceptions.BadRequest({
+        message: `You already pair the roleId and menuId. It should not repeat`,
+      });
+    }
+
+    throw err;
   }
 }
 
