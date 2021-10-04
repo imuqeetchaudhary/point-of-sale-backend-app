a90e3689bb253c378194fd4624675971e54fe450
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2021-10-04 11:39:17 +0500

    added bodyparser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const allRoutes = require("./routes");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
