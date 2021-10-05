31be59dad81d1e3f18835ee821efcbf3590de5ea
Jazim-Abbas <jazimabbas87@gmail.com>
2021-10-05 11:54:14 +0500

    added seed file

---
+++ src/db/seed.sql
@@ -0,0 +1,25 @@
+
+
+insert into
+ad_role(role_id, description, created_by, updated_by, created_at, updated_at)
+values 
+	(1, 'Accountant', 1, 1, now(), now()),
+    (2, 'Manager', 1, 1, now(), now());
+
+insert into
+ad_menu(menu_id, description, link, parent_id,created_by, updated_by, created_at, updated_at)
+values 
+	(1, 'Programming', '/programming', DEFAULT,1, 1, now(), now()),
+    (2, 'Python', '/python', 1, 1, 1, now(), now()),
+    (3, 'Django', '/django', 2, 1, 1, now(), now()),
+    (4, 'PHP', '/php', 1, 1, 1, now(), now());
+
+insert into 
+ad_menu_access_roles (role_id, menu_id, created_by, updated_by, created_at, updated_at)
+values 
+	(1, 1, 1, 1, now(), now()),
+    (1, 3, 1, 1, now(), now()),
+    
+    (2, 1, 1, 1, now(), now()),
+    (2, 2, 1, 1, now(), now());
+     (2, 3, 1, 1, now(), now())
