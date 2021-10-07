1c26ca8e42f19fcc8dc612e7d8a236b6d9306450
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2021-10-07 11:38:44 +0500

    implementation of user registration with roleIds

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,38 +1,31 @@
 const userService = require("../services/user");
-const userAccessRoleService = require("../services/user-access-roles/index");
+const { promise } = require("../middlewares/promise");
 const Exceptions = require("../utils/custom-exceptions");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-exports.register = async (req, res) => {
+exports.register = promise(async (req, res) => {
   const { email, password, displayName, roleIds } = req.body;
+  const createdBy = req.user.userId;
 
   const hashPassword = bcrypt.hashSync(password, 10);
 
-  const user = await userService.createUser({
-    displayName,
+  const user = await userService.saveUser({
     email,
     password: hashPassword,
+    displayName,
+    createdBy,
+    roleIds,
   });
-
-  const userId = user.userId;
-  const createdBy = req.user.userId;
-
-  const userRoles = roleIds.map((roleId) => {
-    return { userId, roleId, createdBy };
-  });
-
-  const role = await userAccessRoleService.saveUserRoles({ userRoles });
 
   res.status(200).json({
-    message: "Successfully created new user and user access role",
+    message: "Successfully created new user",
     user,
-    role,
   });
-};
+});
 
-exports.login = async (req, res) => {
+exports.login = promise(async (req, res) => {
   const { email, password } = req.body;
   const user = await userService.findByEmail({ email });
 
@@ -70,12 +63,12 @@ exports.login = async (req, res) => {
     isAdmin: user.isAdmin,
     isSuperuser: user.isSuperuser,
   });
-};
+});
 
-exports.profile = async (req, res) => {
+exports.profile = promise(async (req, res) => {
   const { id } = req.params;
 
   const user = await userService.findById({ id });
 
   res.status(200).json({ user });
-};
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const testRoutes = require("./test");
 const router = express.Router();
 
 const userRoute = require("../routes/user");
@@ -8,7 +7,6 @@ const menuRoute = require("../routes/menu");
 const brandRoute = require("../routes/brand/brand");
 const itemCategoryRoute = require("../routes/item-category/item-category");
 
-router.use("/test", testRoutes);
 
 router.get("/", (req, res) =>
   res.send({ message: "Point Of Sale Backend App RestApi" })
